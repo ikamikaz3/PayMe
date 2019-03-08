1530d2ca4caa1d78181f3ad3c062e49d3949281d
Mohamed <mohamedboufraine@MacBook-Pro-de-Mohamed.local>
2019-03-08 17:05:30 +0100

    ajout du code pour upload l´image

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,14 +1,51 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { Text, View, Button } from "react-native";
+import { Image, Text, View, Button, Alert } from "react-native";
 import { Logout } from "../redux/actions/actionCreators";
+//import { RNCamera, FaceDetector } from 'react-native-camera';
+import{ ImagePicker } from 'expo';
+import { Permissions } from 'expo';
+import * as firebase from 'firebase';
+
+onChooseImagePress =  async () => {
+
+  //const response = await Permissions.askAsync(Permissions.CAMERA);
+  const permission = await Permissions.getAsync(Permissions.CAMERA_ROLL);
+
+  let result = await ImagePicker.launchCameraAsync();
+
+  if (permission.status === 'granted'){
+   if(!result.cancelled) {
+     uploadImage(result.uri, "photo_profile")
+     .then(() => {
+       Alert.alert("Votre photo a bien été prise");
+     })
+
+     .catch((error) => {
+       Alert.alert(error);
+     });
+   }
+  }
+}
+  uploadImage = async (uri, imageFileName) => {
+    const response = await fetch(uri);
+    const blob = await response.blob();
+
+    let ref = firebase.storage.ref().child("photo_profile/" + imageFileName);
+    return ref.put(blob);
+  }
 
 const Profile = props => {
   const { logoutAction } = props;
+  
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Profile Screen</Text>
+      <Button title="Appuyer ici pour vous prendre en photos"
+      onPress={onChooseImagePress}>
+      </Button>
       <Button title="Logout" onPress={() => logoutAction()} />
     </View>
   );
